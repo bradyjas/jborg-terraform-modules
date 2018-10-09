ffea9c324933894aad070808d32ef7b4c896a8e0
Jason Brady <bradyjas@gmail.com>
2018-10-09 14:32:34 -0700

    Breaking change: Updated output names to be less ambiguous. Also added CIDR outputs.

--- common/aws/vpc/outputs.tf
+++ common/aws/vpc/outputs.tf
@@ -5,12 +5,22 @@ output "vpc_id" {
   value       = "${aws_vpc.this.id}"
 }
 
-output "public_subnets" {
+output "public_subnet_ids" {
   description = "List of public subnet IDs"
   value       = ["${aws_subnet.public.*.id}"]
 }
 
-output "private_subnets" {
+output "public_subnet_cidr_blocks" {
+  description = "List of public CIDR blocks"
+  value       = ["${aws_subnet.public.*.cidr_block}"]
+}
+
+output "private_subnet_ids" {
   description = "List of private subnet IDs"
   value       = ["${aws_subnet.private.*.id}"]
+}
+
+output "private_subnet_cidr_blocks" {
+  description = "List of private CIDR blocks"
+  value       = ["${aws_subnet.private.*.cidr_block}"]
 }
